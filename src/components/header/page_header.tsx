3ee46eaa7eeff9e7c7e76853dd3060c95d6022f3
fireclaws9 <41904540+fireclaws9@users.noreply.github.com>
2022-01-23 20:25:18 +0800

    Added Responsive to Components

--- src/components/header/page_header.tsx
+++ src/components/header/page_header.tsx
@@ -4,7 +4,19 @@ import "./page_header.css";
 export default class PageHeader extends Component {
 	render() {
 		return <div className="page_header">
-			<p>Skript Editor</p>
+			<div className="page_header_title">
+				<p>Skript Studio</p>
+			</div>
+			<div className="page_header_subtitle">
+				<p>made with </p>
+				<i className="fas fa-heart"></i>
+				<p> by RuntimeCloud.com</p>
+			</div>
 		</div>;
 	}
 }
+
+//			<div className="page_header_icons">
+//				<p>GitHub</p>
+//				<i className="fab fa-github"></i>
+//			</div>
